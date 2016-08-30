ecff2c758953ee24da2c704c2608714020df696a
Bartek Szopka <bartek.szopka+github@gmail.com>
2016-08-30 17:07:45 +0200

    Header component no longer sets className directly on NavLink

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,22 +3,25 @@ import { Link } from 'react-router';
 
 import NavLink from '../navlink';
 
-import style from './header.css';
+import styles from './header.css';
 
 import logo from './ubuntu-logo.svg';
 
 export default class Header extends Component {
   render() {
     return (
-      <div className={ style.header }>
-        <nav className={ style.container }>
-        <Link className={ style.logo } to="/">
-          <img src={ logo } width="107" height="24" alt="Ubuntu" />
-        </Link>
-        <NavLink className={ style.link } to="/" onlyActiveOnIndex={ true }>Home</NavLink>
-        <NavLink className={ style.link } isLast={true} to="/about">About</NavLink>
-
-        <NavLink className={ style.login } isLast={true} to="/login">Login</NavLink>
+      <div className={ styles.header }>
+        <nav className={ styles.container }>
+          <Link className={ styles.logo } to="/">
+            <img src={ logo } width="107" height="24" alt="Ubuntu" />
+          </Link>
+          <div className={ styles.mainNav }>
+            <NavLink to="/" onlyActiveOnIndex={ true }>Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+          </div>
+          <div className={ styles.sideNav }>
+            <NavLink to="/login">Login</NavLink>
+          </div>
         </nav>
       </div>
     );
--- src/components/navlink/index.js
+++ src/components/navlink/index.js
@@ -1,17 +1,10 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import styles from './navlink.css';
 
 export default class NavLink extends Component {
   render() {
-    let { isLast, className, ...other } = this.props;
-
-    return <Link {...other} className={ `${className} ${isLast ? styles.lastLink : styles.link}` } activeClassName={ styles.active } />;
+    return <Link {...this.props} className={ styles.link } activeClassName={ styles.active } />;
   }
 }
-
-NavLink.propTypes = {
-  isLast: PropTypes.bool,
-  className: PropTypes.string
-};
