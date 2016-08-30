8f15fdf6336f7ac5cae131aa4be0ba34422c9814
Bartek Szopka <bartek.szopka+github@gmail.com>
2016-08-30 16:30:17 +0200

    Moved ubuntu-logo.svg to header module and loading it locally with url-loader

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,13 +4,15 @@ import NavLink from '../navlink/navlink';
 
 import style from './header.css';
 
+import logo from './ubuntu-logo.svg';
+
 export default class Header extends Component {
   render() {
     return (
       <div className={ style.header }>
         <nav className={ style.container }>
         <Link className={ style.logo } to="/">
-          <img src="/img/ubuntu-logo.svg" width="107" height="24" alt="Ubuntu" />
+          <img src={ logo } width="107" height="24" alt="Ubuntu" />
         </Link>
         <NavLink className={ style.link } to="/" onlyActiveOnIndex={ true }>Home</NavLink>
         <NavLink className={ style.link } isLast={true} to="/about">About</NavLink>
