a63ce49177bf2a560191c5b79c6c29af5882d98f
Katie Fenn <katie.fenn@googlemail.com>
2016-09-06 13:59:37 +0100

    39: Fix unit tests

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -1,6 +1,4 @@
-const INITIATE_LOGIN = 'auth/INITIATE_LOGIN';
 const COMPLETE_LOGIN = 'auth/COMPLETE_LOGIN';
-const FAIL_LOGIN = 'auth/FAIL_LOGIN';
 
 const initialState = {
   loggedIn: false
@@ -8,17 +6,13 @@ const initialState = {
 
 export default function reducer(state = initialState, action = {}) {
   switch(action.type) {
-    case INITIATE_LOGIN:
-      return state;
     case COMPLETE_LOGIN:
       return {
         ...state,
         loggedIn: true
       };
-    case FAIL_LOGIN:
-      return state;
     default:
-      return state;
+      return { ...state };
   }
 }
 
