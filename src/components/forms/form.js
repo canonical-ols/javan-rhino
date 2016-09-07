0fc0e9ca07aa96e3714f9c423b902ca854d59eaf
Bartek Szopka <bartek.szopka+github@gmail.com>
2016-09-07 13:17:47 +0200

    Removed unnecessary form.css

--- src/components/forms/form.js
+++ src/components/forms/form.js
@@ -1,9 +1,7 @@
 import React, { PropTypes } from 'react';
 
-import style from './form.css';
-
 export default function Form(props) {
-  return <form onSubmit={ props.onSubmit } className={ style.form }>
+  return <form onSubmit={ props.onSubmit }>
     { props.children }
   </form>;
 }
