3401da5fc6a9aa631d7fe51d3c9f1b36a29eaffb
Stephen Stewart <stephen.stewart@carisenda.com>
2016-09-13 11:07:33 +0100

    add simple implementation of aync actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,70 @@
+import 'whatwg-fetch';
+
 export const COMPLETE_LOGIN = 'COMPLETE_LOGIN';
 
-// FIXME do we need this?
 export function authenticateUser(isAuthenticated, isDev, name) {
   return {
     type: COMPLETE_LOGIN,
     isAuthenticated,
     isDev,
     name
+  };
+}
+
+export const SEND_STRIPE_TOKEN = 'SEND_STRIPE_TOKEN';
+
+export function sendStripeToken(token) {
+  return {
+    type: SEND_STRIPE_TOKEN,
+    token
+  };
+}
+
+export const SEND_STRIPE_TOKEN_SUCCESS = 'SEND_STRIPE_TOKEN_SUCCESS';
+export const SEND_STRIPE_TOKEN_FAILURE= 'SEND_STRIPE_TOKEN_FAILURE';
+
+export function sendStripeTokenSuccess(json) {
+  return {
+    type: SEND_STRIPE_TOKEN_SUCCESS,
+    tos_accepted: json.tos_accepted
+  };
+}
+
+export function sendStripeTokenFailure(errors) {
+  return {
+    type: SEND_STRIPE_TOKEN_FAILURE,
+    errors
+  };
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
+/**
+ * POST a customer record for the user in Ubuntu Pay
+ * @arg {string} token stripe token
+ */
+export function postStripeToken(token) {
+  return (dispatch) => {
+    dispatch(sendStripeToken(token));
+
+    // TODO try-sandbox :(
+    return fetch('/api/purchases/customers', {
+      method: 'POST',
+      body: JSON.stringify({
+        token
+      })
+    })
+      .then(checkStatus)
+      .then(response => response.json())
+      .then(json => dispatch(sendStripeTokenSuccess(json)))
+      .catch(errors => dispatch(sendStripeTokenFailure(errors)));
   };
 }
--- src/containers/add-card.js
+++ src/containers/add-card.js
@@ -1,15 +1,41 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
 
+import { postStripeToken } from '../actions/';
 import PaymentsForm from '../components/payments-form';
 
 import styles from './container.css';
 
-export default class AddCard extends Component {
+export class AddCard extends Component {
+  handleClick() {
+    console.log('aaa');
+    const { dispatch } = this.props;
+    dispatch(postStripeToken('hello-ricardo'));
+  }
+
   render() {
+    const boundClick = this.handleClick.bind(this);
     return (
       <div className={ styles.container }>
         <PaymentsForm />
+        <button onClick={ boundClick }>Test Send Stripe Token</button>
       </div>
     );
   }
 }
+
+AddCard.propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  const {
+    sendStripeToken
+  } = state;
+
+  return {
+    sendStripeToken
+  };
+}
+
+export default connect(mapStateToProps)(AddCard);
