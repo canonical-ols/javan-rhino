dc3c5057526cc1edc8b656c14f3d0a31bcc578f5
Bartek Szopka <bartek.szopka+github@gmail.com>
2016-09-13 18:53:50 +0200

    Added form fields to form state, added simple validation to text inputs

--- src/components/forms/selectField.js
+++ src/components/forms/selectField.js
@@ -6,13 +6,13 @@ import Select from './select';
 import styles from './inputField.css';
 
 export default function SelectField(props) {
-  const { name, label, size='full', options } = props;
+  const { name, label, size='full', required, options } = props;
   const id = `ID_SELECT_FIELD_${name}`;
 
   const className = `${styles.inputField} ${styles[size]}`;
   return <div className={ className }>
     <Label htmlFor={ id }>{ label }:</Label>
-    <Select id={ id } name={ name }>
+    <Select id={ id } name={ name } required={ required } value={ props.value } onChange={ props.onChange }>
       {options.map(function(option){
         return <option key={ `${id}_${option.value}`} value={ option.value }>{ option.name }</option>;
       })}
@@ -24,5 +24,10 @@ SelectField.propTypes = {
   name: PropTypes.string,
   label: PropTypes.string,
   options: PropTypes.array,
-  size: PropTypes.oneOf(['full', 'small'])
+  required: PropTypes.bool,
+  size: PropTypes.oneOf(['full', 'small']),
+  valid: PropTypes.bool,
+  touched: PropTypes.bool,
+  value: PropTypes.string,
+  onChange: PropTypes.func
 };
