edc59adc0bebbe39db9a0373e8899f2f7e6b930c
Bartek Szopka <bartek.szopka+github@gmail.com>
2016-09-19 12:18:31 +0200

    Forcing some JSX code style rules with eslint

--- src/containers/add-card.js
+++ src/containers/add-card.js
@@ -1,5 +1,4 @@
 import React, { PropTypes, Component } from 'react';
-import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
 
 import { postStripeToken } from '../actions/customer';
