aa2f7cd936844d3619fc67ef2e3dd305bfa03d1f
Wes Mason <wes@1stvamp.org>
2016-09-30 14:37:53 +0100

    Add port exposure

--- charm/reactive/java_rhino.py
+++ charm/reactive/java_rhino.py
@@ -2,7 +2,7 @@ from charmhelpers.core import hookenv
 from charmhelpers.core.host import restart_on_change
 from charmhelpers.core.templating import render
 from charms.reactive import when, when_not, set_state
-from charms.ols import code_dir, logs_dir, user
+from charms.ols import check_port, code_dir, logs_dir, port, service_name, user
 
 
 SYSTEMD_CONFIG = '/lib/systemd/system/javan-rhino.service'
@@ -12,7 +12,7 @@ SYSTEMD_CONFIG = '/lib/systemd/system/javan-rhino.service'
 @when('ols.service.installed')
 @restart_on_change({SYSTEMD_CONFIG: ['javan-rhino']}, stopstart=True)
 def configure(cache):
-    deployment = hookenv.config('deployment')
+    environment = hookenv.config('environment')
     session_secret = hookenv.config('session_secret')
     memcache_session_secret = hookenv.config('memcache_session_secret')
     if session_secret and memcache_session_secret:
@@ -24,10 +24,11 @@ def configure(cache):
                 'user': user(),
                 'session_secret': session_secret,
                 'logs_path': logs_dir(),
-                'deployment': deployment,
+                'environment': environment,
                 'cache_hosts': cache.memcache_hosts(),
                 'memcache_session_secret': memcache_session_secret,
             })
+        check_port('ols.{}.express'.format(service_name()), port())
         set_state('service.configured')
     else:
         hookenv.status_set('blocked',
