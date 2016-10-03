7c0594c2ef4837c6244ae15a99233750af91bfbd
Stephen Stewart <stephen.stewart@carisenda.com>
2016-10-04 00:36:06 +0100

    use test from #107

--- test/routes/t_login.js
+++ test/routes/t_login.js
@@ -1,11 +1,17 @@
 import { app } from '../../server/server.js';
 import conf from '../../server/configure.js';
+import nock from 'nock';
 import supertest from 'supertest';
 
 const SSO_HOST = conf.get('SERVER:UBUNTU_SSO_HOST');
+const UBUNTU_SCA_URL = conf.get('SERVER:UBUNTU_SCA_URL');
 const VERIFY_URL = conf.get('SERVER:OPENID:VERIFY_URL');
 
 describe('login routes', () => {
+
+  afterEach(() => {
+    nock.cleanAll();
+  });
 
   describe('authenticate', () => {
     it('should redirect from /login/authenticate to SSO', (done) => {
@@ -22,6 +28,28 @@ describe('login routes', () => {
           new RegExp(encodeURIComponent(VERIFY_URL)),
           done
         );
+    });
+
+    context('when SCA responds with error', () => {
+
+      it('should redirect home on error', (done) => {
+        const sca = nock(UBUNTU_SCA_URL)
+          .post('/dev/api/acl/', {
+            'permissions': ['package_access', 'package_purchase']
+          })
+          .reply(503, '<html>ERROR</html>');
+
+        supertest(app)
+          .get('/login/authenticate')
+          .expect(function(res) {
+            res.body.name = res.body.name.toUpperCase();
+          })
+          .expect('location', '/')
+          .expect(302, (err) => {
+            sca.done();
+            done(err);
+          });
+      });
     });
   });
 });
