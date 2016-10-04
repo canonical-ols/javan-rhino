42a72a2ebc69d56c8567a24a12e49461dfa3dace
Stephen Stewart <stephen.stewart@carisenda.com>
2016-10-04 16:57:11 +0100

    add back devtools hook

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -9,7 +9,8 @@ export default function configureStore(preloadedState) {
     compose(
       applyMiddleware(
         thunk
-      )
+      ),
+      typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
     )
   );
 
