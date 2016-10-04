4a481589632cb038ece2b8963105e6751dbcca4f
Stephen Stewart <stephen.stewart@carisenda.com>
2016-10-04 16:51:46 +0100

    use math

--- server/helpers/session.js
+++ server/helpers/session.js
@@ -4,7 +4,7 @@ import ConnectMemcached from 'connect-memcached';
 const MemcachedStore = ConnectMemcached(session);
 const SESSION_DEFAULTS = {
   cookie: {
-    maxAge: 7.2e+6, // 2 hours, macaroon life span
+    maxAge: 2 * 60 * 60 * 1000, // 2 hours, macaroon life span
   },
   resave: false,
   saveUninitialized: false
