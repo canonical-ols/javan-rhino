148bef0f09a3388abc3ae64b87de81df22b5e078
Stephen Stewart <stephen.stewart@carisenda.com>
2016-10-13 17:43:08 +0100

    working build

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,3 @@
 require('babel-register');
-
-const WebPackIsomorphicTools = require('webpack-isomorphic-tools');
-const basePath = require('path').resolve(__dirname, '../..');
-
-const webpackIsomorphicTools = new WebPackIsomorphicTools(require('../../webpack/webpack-isomorphic-tools-configuration'))
-  .development(process.env.NODE_ENV !== 'production')
-  .server(basePath, function() {
-    require('./server').default(webpackIsomorphicTools);
-  });
+require('css-modules-require-hook/preset');
+require('./server');
