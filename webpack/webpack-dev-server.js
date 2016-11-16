8bedaedae9365eb7e66dd84cac88d404be7609ce
Stephen Stewart <stephen.stewart@carisenda.com>
2016-11-16 21:44:07 +0000

    webpack output doesn't need talisker logs

--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -4,7 +4,6 @@ const webpack = require('webpack');
 const url = require('url');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware  = require('webpack-hot-middleware');
-const prettyBytes = require('pretty-bytes');
 
 const webpackConfig = require('./dev-config');
 const conf = require('../src/server/configure');
@@ -21,68 +20,15 @@ webpackApp.use(webpackDevMiddleware(compiler, {
   hot: true,
   noInfo: false,
   stats: {
-    colors: false,
+    colors: true,
     chunks: false,
     children: false
   },
   headers: { 'Access-Control-Allow-Origin': '*' },
-  publicPath: webpackConfig.output.publicPath,
-  log: logger.info.bind(logger),
-  reporter: (reporterOptions) => {
-    const { state, stats, options } = reporterOptions;
-
-    const obj = stats.toJson({
-      chunks: false,
-      colors: false,
-      children: false,
-      modules: false,
-      chunkModules: false,
-      source: false
-    });
-
-    if (state) {
-      let displayStats = (!options.quiet && options.stats !== false);
-
-      if (displayStats && !(stats.hasErrors() || stats.hasWarnings()) &&
-        options.noInfo)
-      {
-        displayStats = false;
-      }
-
-      if (displayStats) {
-
-        if (obj.hash) {
-          options.log('stats', {
-            hash: obj.hash,
-            version: obj.version,
-            time: obj.time + 'ms'
-          });
-        }
-
-        if (obj.assets && obj.assets.length > 0) {
-
-          obj.assets.forEach((asset) => {
-            options.log('stats', {
-              asset: asset.name,
-              size: prettyBytes(asset.size)
-            });
-          });
-        }
-      }
-
-      if (!options.noInfo && !options.quiet) {
-        options.log('bundle is now VALID.');
-      }
-    } else {
-      options.log('bundle is now INVALID.');
-    }
-  }
+  publicPath: webpackConfig.output.publicPath
 }));
 
-webpackApp.use(webpackHotMiddleware(compiler, {
-  log: logger.info.bind(logger)
-}));
-
+webpackApp.use(webpackHotMiddleware(compiler));
 webpackApp.use(Express.static('public'));
 
 const port = webpackDevUrl.port;
