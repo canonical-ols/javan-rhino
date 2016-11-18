7a7ed201f098d66d050bd4694be09b59d98d5f6a
Stephen Stewart <stephen.stewart@carisenda.com>
2016-11-18 15:40:40 +0000

    add service status endpoints

--- src/server/routes/servicestatus.js
+++ src/server/routes/servicestatus.js
@@ -3,26 +3,35 @@
  * TODO make this a standalone module
  */
 import { Router } from 'express';
+import Memcached from 'memcached';
+
+import conf from './../configure.js';
 
 export const ping = (req, res) => {
   res.send('Ok');
 };
 
 export const check = (req, res) => {
-  res.status(200);
+  const memcached = new Memcached(conf.get('SESSION_MEMCACHED_HOST'));
+
+  memcached.version((err, result) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    res.send(JSON.stringify(result));
+    memcached.end();
+  });
 };
 
-// TODO sentry server side
-// export const error = (req, res) => {
-//  res.status(200);
-// };
-//
-// TODO statsd
-// export const metric = (req, res) => {
-//   res.send(200);
-// };
-//
+export const error = () => {
+  throw new Error('This is a servicestatus error test');
+};
 
+// TODO statsd
+//export const metric = (req, res) => {
+//  res.send(200);
+//};
 
 const router = Router();
 
@@ -30,7 +39,7 @@ router.get('/ping', ping);
 router.head('/ping', ping);
 router.options('/ping', ping);
 router.get('/check', check);
-//router.get('/error', error);
+router.get('/error', error);
 //router.get('/metric', metric);
 
 export default router;
