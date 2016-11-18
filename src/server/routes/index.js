cff810a20354922aa6220f2abe402e87e1c6add3
Stephen Stewart <stephen.stewart@carisenda.com>
2016-11-18 16:36:35 +0000

    fix failing test - missing mocked route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,8 +1,12 @@
+import status from './status';
 import login from './login';
 import api from './api';
 import universal from './universal';
 
+console.log('asas');
+
 export {
+  status,
   login,
   api,
   universal
