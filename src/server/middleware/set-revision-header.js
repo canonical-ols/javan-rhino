14e711dac49125bbe878dd7d633f9d8d1bf7303c
Stephen Stewart <stephen.stewart@carisenda.com>
2016-11-21 13:30:04 +0000

    tidy up

--- src/server/middleware/set-revision-header.js
+++ src/server/middleware/set-revision-header.js
@@ -2,18 +2,17 @@ import fs from 'fs';
 
 // location and name of file is consistent across projects
 const filename = './version-info.txt';
-let revision;
+let revision = 'unknown';
 
 try {
+  // git rev-parse HEAD > version-info.txt
   revision = fs.readFileSync(filename, 'utf8').trim();
+  // logger.debug(`Got X-VCS-revision as ${revision}`);
 } catch(e) {
-  // TODO logger
-  // console.log(`missing ${filename} cannot set X-Vcs-revision)`);
+  // logger.info(`missing ${filename} cannot set correct X-VCS-revision)`);
 }
 
 export default (req, res, next) => {
-  if (revision && !/\s/.test(revision)) {
-    res.set('X-VCS-Revision', revision);
-  }
+  res.set('X-VCS-Revision', revision);
   next();
 };
