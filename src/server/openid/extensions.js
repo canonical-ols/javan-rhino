be86590e7032873edd545b1215fc55387a0a88dc
Colin Watson <cjwatson@debian.org>
2018-10-17 08:04:17 +0000

    Remove OpenID teams extension and configuration
    This hasn't been used since commit
    d41e6943ae71904276041ec213d9a7759d5cefe3.

--- src/server/openid/extensions.js
+++ src/server/openid/extensions.js
@@ -1,20 +1,3 @@
-export class Teams {
-  constructor(teams) {
-    this.requestParams = {
-      'openid.ns.lp': 'http://ns.launchpad.net/2007/openid-teams'
-    };
-    if (teams && teams.length) {
-      this.requestParams['openid.lp.query_membership'] = teams.join(',');
-    }
-  }
-
-  fillResult(params, result) {
-    if (params['openid.lp.is_member'] && params['openid.lp.is_member'].length) {
-      result['teams'] = params['openid.lp.is_member'].split(',');
-    }
-  }
-}
-
 export class Macaroons {
   constructor(cid) {
     this.requestParams = {
